0da974e54d5dd67a63fd2fea9da012eca7352fb2
mschmal2 <maximilian_dirk.schmalenbach@smail.th-koeln.de>
2022-07-18 15:48:09 +0200

    added quality

--- src/CCapture.js
+++ src/CCapture.js
@@ -516,7 +516,7 @@ function CCGIFEncoder( settings ) {
 
   	this.encoder = new GIF({
 		workers: settings.workers,
-		quality: 10,
+		quality: settings.quality,
 		workerScript: settings.workersPath + 'gif.worker.js',
 		transparent: settings.colorToIgnor
 	} );
